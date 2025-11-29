97e21935e6b5445cc1423f031ee52f70b59d69a8
Adriāns Jērcītis <adriansjercitis1@gmail.com>
2025-11-29 12:37:59 +0200

    Add static task list and display in members div

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-// Example: simple console log
-console.log("IT Arodbiedrības projekts veiksmīgi ielādēts!");
+// Static task list
+const tasks = [
+  "Jānis Bērziņš – Praktikants",
+  "Projekta uzdevums 1 – Pabeigt līdz 01.12",
+  "Projekta uzdevums 2 – Testa ieraksts"
+];
+
+const div = document.getElementById("members");
+div.innerHTML = tasks.map(t => `<p>${t}</p><hr>`).join("");
